6794e14230f72eb80afc549cb820ba58f3c4d21a
Diego Hauenstein <diegotr4de@gmail.com>
2026-05-04 21:00:50 -0300

    adiciona login jwt

--- oficina_backend/settings.py
+++ oficina_backend/settings.py
@@ -100,8 +100,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
--- oficina_backend/urls.py
+++ oficina_backend/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from core.views import ClienteViewSet, VeiculoViewSet, OrdemServicoViewSet
 
@@ -11,5 +12,9 @@ router.register(r'ordens', OrdemServicoViewSet, basename='ordens')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('api/', include(router.urls)),
 ]
