cf512bde4f04345ac4a879b3ee7cee6c412a873a
Diego Hauenstein <diegotr4de@gmail.com>
2026-05-14 17:09:45 -0300

    corrige exibicao de imagens media

--- oficina_backend/urls.py
+++ oficina_backend/urls.py
@@ -1,59 +1,30 @@
-from django.contrib import admin
-from django.urls import path, include
-
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
+
+from django.contrib import admin
+from django.urls import path, include, re_path
 
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
-from core.views import (
-    ClienteViewSet,
-    VeiculoViewSet,
-    OrdemServicoViewSet,
-)
+from core.views import ClienteViewSet, VeiculoViewSet, OrdemServicoViewSet
 
 router = DefaultRouter()
-
-router.register(
-    r'clientes',
-    ClienteViewSet,
-    basename='clientes',
-)
-
-router.register(
-    r'veiculos',
-    VeiculoViewSet,
-    basename='veiculos',
-)
-
-router.register(
-    r'ordens',
-    OrdemServicoViewSet,
-    basename='ordens',
-)
+router.register(r'clientes', ClienteViewSet, basename='clientes')
+router.register(r'veiculos', VeiculoViewSet, basename='veiculos')
+router.register(r'ordens', OrdemServicoViewSet, basename='ordens')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path(
-        'api/login/',
-        TokenObtainPairView.as_view(),
-        name='token_obtain_pair',
-    ),
-
-    path(
-        'api/token/refresh/',
-        TokenRefreshView.as_view(),
-        name='token_refresh',
-    ),
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('api/', include(router.urls)),
-]
 
-# SERVIR IMAGENS
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT},
+    ),
+]
